a4e6e8c3738414a8f8432f421d158a522cce231f
Tugce Dalay <tgcdalay@gmail.com>
2024-08-29 01:35:57 +0300

    core

---
+++ eCommerceSimulation/src/eCommerceSimulation/core/MemberLoginManagerAdapter.java
@@ -0,0 +1,13 @@
+package eCommerceSimulation.core;
+
+import eCommerceSimulation.entities.concretes.User;
+
+public class MemberLoginManagerAdapter implements MemberService{
+
+	@Override
+	public void memberToSystem(User user) {
+		System.out.println("");
+		
+	}
+       
+}
---
+++ eCommerceSimulation/src/eCommerceSimulation/core/MemberService.java
@@ -0,0 +1,7 @@
+package eCommerceSimulation.core;
+
+import eCommerceSimulation.entities.concretes.User;
+
+public interface MemberService {
+    public void memberToSystem(User user);
+}
